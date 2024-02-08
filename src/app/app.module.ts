15191799e1442c0ed0d5513a586035a5e5471dc1
Márcio Andrade <marcio.andrade@gmail.com>
2024-02-08 16:04:18 -0300

    Criado o cabeçalho e rodapé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './Components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './Components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
